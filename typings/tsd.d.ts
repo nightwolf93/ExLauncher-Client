a93ab362b46e140ce11cab19552fa42231c72616
Nicolas MARTINS <nmartins@atnetplanet.com>
2015-06-03 11:20:08 +0200

    add some typings

---
+++ typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="greensock/greensock.d.ts" />
+/// <reference path="backbone/backbone.d.ts" />
+/// <reference path="underscore/underscore.d.ts" />
